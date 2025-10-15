33e1e4dd0124f68b52ebcfd5fcfc1c9d170b77ed
SUNMI <hariaus@naver.com>
2025-10-15 20:39:21 +0900

    test: 테스트 코드 리팩토링 - 메소드 이름 변경 - 기댓값, 실제 값 변수 추가 - 테스트 설명 변경

--- src/test/java/calculator/calculatorTest/CalculatorTest.java
+++ src/test/java/calculator/calculatorTest/CalculatorTest.java
@@ -11,17 +11,18 @@ import java.util.List;
 
 public class CalculatorTest {
 
-    @DisplayName("계산기 리스트 덧셈 테스트")
+    @DisplayName("양수 리스트를 더하면 올바른 합을 반환한다.")
     @Test
-    void calculatorAddTest() {
+    void addPositiveNumbersInList() {
         // given(준비)
         final Calculator calculator = new Calculator();
         final List<Long> list = new ArrayList<>(Arrays.asList(1L, 2L, 3L));
+        final long expected = 6L;
 
         // when(실행)
-        final long answer = calculator.add(list);
+        final long actual = calculator.add(list);
 
         // then(검증)
-        assertEquals(6L, answer);
+        assertEquals(expected, actual);
     }
 }
