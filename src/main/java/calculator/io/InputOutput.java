7ce6489bc4db75934886ea55a67ab34d2c5c3dc9
SUNMI <hariaus@naver.com>
2025-10-16 19:40:31 +0900

    feat: 입출력 객체 추가

---
+++ src/main/java/calculator/io/InputOutput.java
@@ -0,0 +1,14 @@
+package calculator.io;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputOutput {
+
+    public String input() {
+        return Console.readLine();
+    }
+
+    public void output(Long input) {
+        System.out.println(input);
+    }
+}
