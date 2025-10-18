594be714925fcd47869c8275dac4f070968ba3ef
SUNMI <hariaus@naver.com>
2025-10-19 00:55:25 +0900

    fix: 숫자 문자열에서 두자리 숫자를 인식 못하는 문제 해결 - 정규표현식에 \d -> \d+ 로 수정하여 두자리 숫자 인식 가능하게 변경

--- src/main/java/calculator/check/Check.java
+++ src/main/java/calculator/check/Check.java
@@ -3,12 +3,14 @@ package calculator.check;
 public class Check {
 
     public void checkFormat(String inputString) {
-        if (!inputString.matches("(//[^0-9]*\\\\n)?((\\d.)*\\d)?")) {
+
+        if (!inputString.matches("(//[^0-9]*\\\\n)?((\\d+.)*\\d*)?")) {
             throw new IllegalArgumentException();
         }
     }
 
     public void checkNumberFormat(String inputString, String regExp) {
+
         regExp = "((\\d" + regExp + "*)*\\d)?";
         if (!inputString.matches(regExp)) {
             throw new IllegalArgumentException();
